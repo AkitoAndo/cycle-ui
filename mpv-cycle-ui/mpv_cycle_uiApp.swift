c7618b75f7e4c3c04ab3392bd7a6af605fec8b60
AkitoAndo <28ww.lo.ol.ww28@gmail.com>
2025-04-13 13:32:42 +0900

    Initial Commit

---
+++ mpv-cycle-ui/mpv_cycle_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mpv_cycle_uiApp.swift
+//  mpv-cycle-ui
+//
+//  Created by Akito Ando on 2025/04/13.
+//
+
+import SwiftUI
+
+@main
+struct mpv_cycle_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mpv-cycle-uiTests/mpv_cycle_uiTests.swift
@@ -0,0 +1,16 @@
+//
+//  mpv_cycle_uiTests.swift
+//  mpv-cycle-uiTests
+//
+//  Created by Akito Ando on 2025/04/13.
+//
+
+import Testing
+
+struct mpv_cycle_uiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
