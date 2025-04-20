1b569f064b21a143aa878bedae71efc6ef873094
AkitoAndo <28ww.lo.ol.ww28@gmail.com>
2025-04-20 15:52:40 +0900

    Initial Commit

---
+++ CycleMVP/CycleMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleMVPApp.swift
+//  CycleMVP
+//
+//  Created by Akito Ando on 2025/04/20.
+//
+
+import SwiftUI
+
+@main
+struct CycleMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleMVPTests/CycleMVPTests.swift
@@ -0,0 +1,16 @@
+//
+//  CycleMVPTests.swift
+//  CycleMVPTests
+//
+//  Created by Akito Ando on 2025/04/20.
+//
+
+import Testing
+
+struct CycleMVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
